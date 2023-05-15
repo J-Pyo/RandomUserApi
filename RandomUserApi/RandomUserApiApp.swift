eab9c9707ad747271f4305ca1778029a27bbac68
Jeongpyo <jeongpyo6223@gmail.com>
2023-05-15 14:27:14 +0900

    Initial Commit

---
+++ RandomUserApi/RandomUserApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUserApiApp.swift
+//  RandomUserApi
+//
+//  Created by 홍정표 on 2023/05/15.
+//
+
+import SwiftUI
+
+@main
+struct RandomUserApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
